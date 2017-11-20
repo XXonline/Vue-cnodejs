32019a358be833058121931773da3b728b432dc6
zhangchunxian <xxonline0823@gmail.com>
2017-11-21 00:32:19 +0800

    修改项目目录

---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="item-box">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+ @import "../assets/less/core/index.less";
+</style>
